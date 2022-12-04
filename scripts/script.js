ab9eb8388d07d9edf7001909f1ef10d6325ec047
Albihoti <100148161+Albihoti@users.noreply.github.com>
2022-12-05 00:58:01 +0100

    login, register, profile page

---
+++ scripts/script.js
@@ -0,0 +1,34 @@
+
+
+
+// function login(){
+     
+// const userName = document.getElementById("exampleInputEmail1").value
+// const password = document.getElementById("exampleInputPassword1").value
+// const rawBody = JSON.stringify({
+//   "id":"111",
+//   "username":"R111e2oan",
+//   "email":"alb111i3472@te1st.com",
+//   "password":"alo"
+// })
+// const configMethod= { 
+//   method:'POST',
+//   headers:{'Content-Type':'application/stream+json','Accept':'application/json' },
+//   mode:'no-cors',
+//   body:rawBody
+// }
+
+//   fetch('http://localhost:8080/perdoruesi/register',configMethod)
+//         }
+
+
+// function login(){
+//   fetch('http://localhost:8080/hello', {method:'GET', headers:
+//   {'Content-type':'application/json'}, mode:'no-cors'}).then(response => {
+//   console.log(typeof response)
+// console.log(response)}).then(data => {
+//   console.log(data)
+//   console.log(typeof data)
+//  })
+
+// }
